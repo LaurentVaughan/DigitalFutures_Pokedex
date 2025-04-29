46207cc803f1f445f382b7884803519eb1f721f0
Sam <Sammz@users.noreply.github.com>
2025-04-29 11:17:05 +0100

    add select and image in app

--- pokemon_streamlit_app.py
+++ pokemon_streamlit_app.py
@@ -1,8 +1,17 @@
 import streamlit as st
 import plotly.express as px
 import pandas as pd
+from selector_function import select_pokemon_number
 
 data = "assets/pokemon.csv"
 df = pd.read_csv(data)
 st.title("Pokemon Dataset Explorer")
-st.table(df)
+# st.table(df)
+
+
+
+pokemon_number = select_pokemon_number()
+
+image_url = f"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{pokemon_number}.png"
+
+st.image(image_url)
