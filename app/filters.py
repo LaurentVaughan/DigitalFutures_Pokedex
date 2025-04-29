78a0075e6079b77c385bb20af19fe19a81b64df0
Ashleigh <150076417+AshleighSham@users.noreply.github.com>
2025-04-29 11:23:55 +0100

    im probably making a mess

--- app/filters.py
+++ app/filters.py
@@ -1,35 +1,6 @@
 import streamlit as st
 
-
-def add_passenger_class_filter(df):
-    """Add a filter for Passenger Class."""
-    return st.sidebar.multiselect(
-        "Select Passenger Class",
-        options=sorted(df["Pclass"].unique()),
-        default=sorted(df["Pclass"].unique()),
-    )
-
-
-def add_embarked_port_filter(df):
-    """Add a filter for Embarked Port."""
-    return st.sidebar.multiselect(
-        "Select Embarked Port",
-        options=sorted(df["Embarked"].unique()),
-        default=sorted(df["Embarked"].unique()),
-    )
-
-
-def filter_dataframe(df, pclass_filter, embarked_filter):
-    """Filter the DataFrame based on selected filters."""
-    return df[
-        (df["Pclass"].isin(pclass_filter))
-        & (df["Embarked"].isin(embarked_filter))
-    ]
-
-
 def apply_filters(df):
     """Apply all filters and return the filtered DataFrame."""
     st.sidebar.header("Filters")
-    pclass_filter = add_passenger_class_filter(df)
-    embarked_filter = add_embarked_port_filter(df)
-    return filter_dataframe(df, pclass_filter, embarked_filter)
+    return df
--- pokemon_streamlit_app.py
+++ pokemon_streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import plotly.express as px
 import pandas as pd
+from app.filters import apply_filters
+from selector_function import select_pokemon_number
 
 def main():
     """Main function to run the Streamlit app."""
@@ -13,8 +15,15 @@ def main():
 
     data = "assets/pokemon.csv"
     df = pd.read_csv(data)
+    filtered_df = apply_filters(df)
     st.title("Pokemon Dataset Explorer")
-    st.table(df[2:5])
+    st.table(filtered_df[2:5])
+    
+    pokemon_number = select_pokemon_number()
+
+    image_url = f"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{pokemon_number}.png"
+
+    st.image(image_url)
 
 if __name__ == "__main__":
     main()
