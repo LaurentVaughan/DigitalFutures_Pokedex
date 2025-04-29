d3cf2a78f6fb9854f9d4bb633387d2fc32a86e8a
Ashleigh <150076417+AshleighSham@users.noreply.github.com>
2025-04-29 11:01:35 +0100

    styling

--- pokemon_streamlit_app.py
+++ pokemon_streamlit_app.py
@@ -2,7 +2,19 @@ import streamlit as st
 import plotly.express as px
 import pandas as pd
 
-data = "assets/pokemon.csv"
-df = pd.read_csv(data)
-st.title("Pokemon Dataset Explorer")
-st.table(df)
+def main():
+    """Main function to run the Streamlit app."""
+    st.set_page_config(
+        page_title="Podeydex",
+        page_icon="🔴",
+        layout="wide",
+        initial_sidebar_state="auto",
+    )
+
+    data = "assets/pokemon.csv"
+    df = pd.read_csv(data)
+    st.title("Pokemon Dataset Explorer")
+    st.table(df[2:5])
+
+if __name__ == "__main__":
+    main()
